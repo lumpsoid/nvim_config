6ada26d997b0ce5fd13aa3d84108cacde983d858
lumpsoid <mmgeeki@gmail.com>
2022-12-02 19:23:57 +0300

    update

--- init.lua
+++ init.lua
@@ -23,3 +23,12 @@ vim.cmd('colorscheme base16-gruvbox-dark-soft')
 vim.cmd[[command! Resethl              lua require'custom_functions'.resethl()]]
 vim.cmd[[command! -bang -nargs=* Fzfnn call fzf#run(fzf#wrap({'source': 'ls -r', 'sink': 'e', 'options': '--preview="cat {}" --preview-window=up,wrap,~1 --multi'},<bang>0))]]
 
+vim.api.nvim_create_autocmd(
+    { "BufWritePost" },
+    { pattern = { "/home/qq/Documents/dwmblocks/config.h" }, command = "!cd /home/qq/Documents/dwmblocks; secret-tool lookup user iurii service sudo | sudo -S make install && { killall -q dwmblocks; setsid dwmblocks & }" }
+)
+
+vim.api.nvim_create_autocmd(
+    { "BufWritePost" },
+    { pattern = { "/home/qq/Documents/dwm/config.h" }, command = "!cd /home/qq/Documents/dwm; secret-tool lookup user iurii service sudo | sudo -S make install && sc-renewwm" }
+)
