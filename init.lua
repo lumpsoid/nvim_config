1d808fc74522ba6f8332887a194a081b3b1f157f
lumpsoid <mmgeeki@gmail.com>
2023-06-10 10:03:53 +0200

    something

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ require('dwm_autocmd')
 
 --vim.cmd('colorscheme base16-brushtrees')
 --vim.cmd('colorscheme base16-tokyo-night-terminal-dark')
-vim.cmd('colorscheme base16-dracula')
+vim.cmd('colorscheme base16-decaf')
 
 vim.cmd[[command! Resethl              lua require'custom_functions'.resethl()]]
 vim.cmd[[command! -bang -nargs=* Fzfnn call fzf#run(fzf#wrap({'source': 'ls -r', 'sink': 'e', 'options': '--preview="cat {}" --preview-window=up,wrap,~1 --multi'},<bang>0))]]
--- lua/dwm_autocmd.lua
+++ lua/dwm_autocmd.lua
@@ -1,11 +1,11 @@
 vim.api.nvim_create_autocmd(
     { "BufWritePost" },
-    { pattern = { "/home/qq/Documents/dwmblocks/blocks.h" }, command = "!cd /home/qq/Documents/dwmblocks; secret-tool lookup user iurii service sudo | sudo -S make install && { killall -q dwmblocks; setsid dwmblocks & }" }
+    { pattern = { "/home/qq/Documents/dwmblocks/blocks.h" }, command = "!cd /home/qq/Documents/dwmblocks; pass sudo | sudo -S make install && { killall -q dwmblocks; setsid dwmblocks & }" }
 )
 
 vim.api.nvim_create_autocmd(
     { "BufWritePost" },
-    { pattern = { "/home/qq/.config/dwm/config.h" }, command = "!cd /home/qq/.config/dwm; secret-tool lookup user iurii service sudo | sudo -S make install && sc-renewwm" }
+    { pattern = { "/home/qq/.config/dwm/config.h" }, command = "!cd /home/qq/.config/dwm; pass sudo | sudo -S make install && sc-renewwm" }
 )
 
 --vim.api.nvim_create_autocmd(
