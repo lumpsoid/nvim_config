6cef61d0bb38fe91acb1ee721c6270ab00737e3c
lumpsoid <mmgeeki@gmail.com>
2024-08-09 00:06:40 +0200

    moved mappings into global file

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,5 @@ return {
         require('oil').setup({
             view_options = { show_hidden = true }
         })
-        vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
     end
 }
