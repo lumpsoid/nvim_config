a41469e46f07d82e747886d56e45f5389b01b708
lumpsoid <mmgeeki@gmail.com>
2024-08-09 00:06:01 +0200

    updated hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,7 +1,7 @@
 -- unmaintained 'phaazon/hop.nvim',
 return {
   'smoka7/hop.nvim',
-  version = '*',
+  version = 'v2',
   opts = {
   --    keys = 'asdfghjkl',
       quit_key = '<ESC>',
@@ -12,14 +12,5 @@ return {
   config = function()
     require('hop').setup()
     -- keymaps
-    vim.keymap.set('n', ',w', ':HopWordMW<CR>', {desc = 'All words in visible buffers'})
-    vim.keymap.set('n', ',j', ':HopLineMW<CR>', {desc = 'two way line'})
-    vim.keymap.set('n', ',p', ':HopPatternMW<CR>', {desc = 'Pattern in all visible buffers'})
-    vim.keymap.set(
-      'n',
-      'gl',
-      [[:lua require("core.hop-custom").hint_wikilink_follow("\\[\\[")<CR>]],
-      {desc = 'Find all notes [[timestamp]]'}
-    )
   end
 }
